2f87e142d722b029f40f28b5564ebd07e2170a55
Robert <rbbenett0614@gmail.com>
2020-12-02 22:55:03 +0000

    First Commit

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+    const horizontalJoin = letters.map(ls => ls.join(''));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word) && word) {
+            return true
+        };
+    };
+    let verticalLetters = [];
+    for (let i = 0; i <letters[0].length; i++) {
+        verticalLetters.push([]);
     }
+    for (let i = 0; i < letters.length; i++) {
+        for( let j = 0; j < letters[i].length; j++) {
+            verticalLetters[j].push(letters[i][j]);
+        }
+    }
+    const verticalJoin = verticalLetters.map(ls => ls.join(''));
+    for (l of verticalJoin) {
+        if (l.includes(word) && word) {
+            return true
+        };
+    };
+    
+
+    return false;    
+    
 }
 
 module.exports = wordSearch
